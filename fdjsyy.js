3a9853d2c51b01371bb81d28d72441b35d184ba0
wxyzGithub <80240075+wxyzGithub@users.noreply.github.com>
2021-06-03 18:26:58 +0800

    Add files via upload

--- fdjsyy.js
+++ fdjsyy.js
@@ -1,16 +1,18 @@
 圈X
 [rewrite_local]
 #樊登读书解锁付费課程
-^https:\/\/api.*.com\/* url script-response-body https://raw.githubusercontent.com/wxyzGithub/js/main/fdjsyy.js
-^https:\/\/gateway-api.*.com(\/resource-orchestration-system\/knowledge\/v100\/mainList|\/resource-orchestration-system\/program\/content) url script-response-body https://raw.githubusercontent.com/wxyzGithub/js/main/fdjsyy.js
+^https:\/\/gateway-api\.dushu365\.com\/smart-orch\/program\/v100\/\* url script-response-body https://raw.githubusercontent.com/wxyzGithub/js/main/fdjsyy.js
+^https:\/\/gateway-api\.dushu365\.com(\/resource-orchestration-system\/knowledge\/v100\/mainList|\/resource-orchestration-system\/program\/content) url script-response-body https://raw.githubusercontent.com/wxyzGithub/js/main/fdjsyy.js
 
 [mitm]
-hostname= gateway-api.*.com,api.*.com
- 
+hostname= gateway-api.dushu365.com
+
 
 
 
 */
+
+
 
 body = $response.body.replace(/isBuyed":\w+/g, 'isBuyed":true').replace(/buyed":\w+/g, 'buyed":true').replace(/trial":\w+/g, 'trial":true').replace(/free":\w+/g, 'free":true').replace(/hasBuy":\w+/g, 'hasBuy":1')                   ;
 $done({body});
