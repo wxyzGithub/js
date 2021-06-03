ac15a97b2eda20010ea6204e7e2e47bb6c7bbde8
wxyzGithub <80240075+wxyzGithub@users.noreply.github.com>
2021-06-03 17:52:44 +0800

    Add files via upload

---
+++ fdjsyy.js
@@ -0,0 +1,18 @@
+圈X
+[rewrite_local]
+#樊登读书解锁付费課程
+^https:\/\/api.*.com\/* url script-response-body https://raw.githubusercontent.com/sngxpro/QuantumultX/master/fdds/sngxfdkc.js
+^https:\/\/gateway-api.*.com(\/resource-orchestration-system\/knowledge\/v100\/mainList|\/resource-orchestration-system\/program\/content) url script-response-body https://raw.githubusercontent.com/sngxpro/QuantumultX/master/fdds/sngxfdkc.js
+
+[mitm]
+hostname= gateway-api.*.com,api.*.com
+
+
+
+
+*/
+
+
+
+body = $response.body.replace(/isBuyed":\w+/g, 'isBuyed":true').replace(/buyed":\w+/g, 'buyed":true').replace(/trial":\w+/g, 'trial":true').replace(/free":\w+/g, 'free":true').replace(/hasBuy":\w+/g, 'hasBuy":1')                   ;
+$done({body});
