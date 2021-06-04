07dac336b0ed77a70074b220e8ba50393ffaf4bb
wxyzGithub <80240075+wxyzGithub@users.noreply.github.com>
2021-06-04 17:49:15 +0800

    fdjsyy.js

--- fdjsyy.js
+++ fdjsyy.js
@@ -1,11 +1,27 @@
-圈X
+#船长著作 2020.06.04
+
+/*
+樊登读书-解锁课程fdjske
+
+MITM = gateway-api.dushu365.com
+
+[rewrite_local]
+^https:\/\/gateway-api\.dushu365\.com*
+
 [rewrite_local]
 #樊登读书解锁付费課程
 ^https:\/\/gateway-api\.dushu365\.com\/smart-orch\/program\/v100\/\* url script-response-body https://raw.githubusercontent.com/wxyzGithub/js/main/fdjsyy.js
-^https:\/\/gateway-api\.dushu365\.com(\/resource-orchestration-system\/knowledge\/v100\/mainList|\/resource-orchestration-system\/program\/content) url script-response-body https://raw.githubusercontent.com/wxyzGithub/js/main/fdjsyy.js
 
-[mitm]
-hostname= gateway-api.dushu365.com
+
+
+var body = $response.body;
+
+
+body=body.replace(/free":\w+/g,'free":true');
+
+
+$done({body});
+
 
 
 
