d7504c13010bddac4531dedbeea193342a671c9f
wxyzGithub <80240075+wxyzGithub@users.noreply.github.com>
2021-06-04 17:50:40 +0800

    Update fdjsyy.js

--- fdjsyy.js
+++ fdjsyy.js
@@ -12,7 +12,7 @@ MITM = gateway-api.dushu365.com
 #樊登读书解锁付费課程
 ^https:\/\/gateway-api\.dushu365\.com\/smart-orch\/program\/v100\/\* url script-response-body https://raw.githubusercontent.com/wxyzGithub/js/main/fdjsyy.js
 
-
+*/
 
 var body = $response.body;
 
@@ -25,10 +25,3 @@ $done({body});
 
 
 
-
-*/
-
-
-
-body = $response.body.replace(/isBuyed":\w+/g, 'isBuyed":true').replace(/buyed":\w+/g, 'buyed":true').replace(/trial":\w+/g, 'trial":true').replace(/free":\w+/g, 'free":true').replace(/hasBuy":\w+/g, 'hasBuy":1')                   ;
-$done({body});
